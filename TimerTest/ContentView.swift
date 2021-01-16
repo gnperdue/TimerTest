8ddc9d1cdfa95c3ef39a95c26f6ad3357ada6250
Gabriel Perdue <gnperdue@hey.com>
2021-01-16 15:28:54 -0600

    Initial Commit

---
+++ TimerTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimerTest
+//
+//  Created by Gabriel Perdue on 1/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimerTest/TimerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerTestApp.swift
+//  TimerTest
+//
+//  Created by Gabriel Perdue on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct TimerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
