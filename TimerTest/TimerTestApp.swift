ce9310515ce74a2e7d812a3ad63f138f6d980df9
Gabriel Perdue <gnperdue@hey.com>
2021-01-16 15:29:47 -0600

    re-indent

--- TimerTest/TimerTestApp.swift
+++ TimerTest/TimerTestApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct TimerTestApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
